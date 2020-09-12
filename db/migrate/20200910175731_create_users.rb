9aa3de234a98ba0a4a951a7065b867cb7af7061b
Sienna Kopf <princess.kopf@gmail.com>
2020-09-12 15:33:33 -0600

    Add denver as default for default location for user

--- db/migrate/20200910175731_create_users.rb
+++ db/migrate/20200910175731_create_users.rb
@@ -6,7 +6,7 @@ class CreateUsers < ActiveRecord::Migration[5.2]
       t.string :access_token
       t.string :refresh_token
       t.string :email
-      t.string :default_location
+      t.string :default_location, default: "denver"
 
       t.timestamps
     end
