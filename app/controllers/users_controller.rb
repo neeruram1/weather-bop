3e769e3aa2c6f0d7a07ab450366af97dedcdba31
Sienna Kopf <princess.kopf@gmail.com>
2020-09-13 14:34:10 -0600

    Add (ugly) conditional logic to the view to determine which location object to send to the facade

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,5 +1,17 @@
 class UsersController < ApplicationController
   def show
-    @weather_music = WeatherMusicFacade.new(current_user.token, current_user.default_location).weather_music
+    if params[:city].nil? == false && params[:country].nil? == false
+      city = params[:city].downcase
+      country = params[:country].downcase
+      if params[:state].nil? == false
+        state = params[:state].downcase
+        location = "#{city}, #{state}, #{country}"
+      else
+        location = "#{city}, #{country}"
+      end
+    else
+      location = current_user.default_location
+    end
+    @weather_music = WeatherMusicFacade.new(current_user.token, location).weather_music
   end
 end
