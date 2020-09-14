a0962f6bad4d6faadc2108eb75d43a4dc3a07d4e
Ashkan Abbasi <airashkan@yahoo.com>
2020-09-14 10:14:19 -0600

    Finish Logout functionality and add change location button

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,4 +4,10 @@ class SessionsController < ApplicationController
     session[:user_id] = @user.id
     redirect_to dashboard_path
   end
+
+  def destroy
+    session[:user_id] = nil
+    redirect_to root_path
+  end
+
 end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   root controller: :welcome, action: :index
   get '/auth/spotify/callback', to: 'sessions#create', as: 'login'
+  get '/logout', to: 'sessions#destroy', as: 'logout'
   get '/auth/failure', to: 'welcome#failure', as: 'login_fail'
   get '/dashboard', to: 'users#show', as: 'dashboard'
 end
