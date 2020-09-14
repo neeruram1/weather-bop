792b96eeb103e8d34c300b2e51deeae51f5f6c28
Josh Tukman <josh.tukman@gmail.com>
2020-09-14 09:14:19 -0600

    Add update action to controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -2,4 +2,22 @@ class UsersController < ApplicationController
   def show
     @weather_music = WeatherMusicFacade.new(current_user.token, current_user.default_location).weather_music
   end
+  def update
+    @user = User.find(current_user.id)
+    if user_params[:state] == ""
+       @user.update(default_location: "#{user_params[:city].strip}, #{user_params[:country]}")
+      @user.save
+    else
+      @user.update(default_location: "#{user_params[:city].strip.downcase}, #{user_params[:state].downcase}, #{user_params[:country].downcase}")
+      @user.save     
+    end
+    redirect_to dashboard_path
+  end 
+  
+  private
+
+  def user_params
+    params.permit(:city, :state, :country)
+  end 
+
 end
