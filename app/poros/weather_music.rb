74dcc51c7de7afdc87c91a1396137e7d78b567e5
Sienna Kopf <princess.kopf@gmail.com>
2020-09-16 11:19:20 -0600

    add main description for use in playlist naming

--- app/poros/weather_music.rb
+++ app/poros/weather_music.rb
@@ -3,6 +3,7 @@ class WeatherMusic
               :forecast_city_name,
               :forecast_country_name,
               :forecast_description,
+              :forecast_main_description,
               :forecast_temp,
               :forecast_temp_min,
               :forecast_temp_max,
@@ -13,6 +14,7 @@ class WeatherMusic
     @forecast_city_name = weather_music_info[:data][:weather][:attributes][:city_name]
     @forecast_country_name = weather_music_info[:data][:weather][:attributes][:country_name]
     @forecast_description = weather_music_info[:data][:weather][:attributes][:description]
+    @forecast_main_description = weather_music_info[:data][:weather][:attributes][:main_description]
     @forecast_temp = weather_music_info[:data][:weather][:attributes][:temp]
     @forecast_temp_min = weather_music_info[:data][:weather][:attributes][:temp_min]
     @forecast_temp_max = weather_music_info[:data][:weather][:attributes][:temp_max]
