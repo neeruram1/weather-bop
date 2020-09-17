07a2ef458699663e9611aa981eaac1e468509bd8
Sienna Kopf <princess.kopf@gmail.com>
2020-09-17 07:53:06 -0600

    Refactor playlists controller to concatonate playlist name in the weather bop and pass to sinatra as one variable

--- app/facades/playlist_facade.rb
+++ app/facades/playlist_facade.rb
@@ -1,15 +1,14 @@
 class PlaylistFacade
-  def initialize(token, tracks, playlist_data)
+  def initialize(token, tracks, playlist_name, playlist_data)
     @token = token
-    @location = playlist_data[:q].downcase
-    @main_description = playlist_data[:main_description].downcase
+    @playlist_name = playlist_name.downcase
     @user_id = playlist_data[:user_id]
     @tracks = tracks
     @playlist_service = PlaylistService.new
   end
 
   def post_playlist
-    @playlist_service.add_playlist_to_library(@token, @location, @main_description, @user_id, @tracks)
+    @playlist_service.add_playlist_to_library(@token, @playlist_name, @user_id, @tracks)
   end
 
   def status
